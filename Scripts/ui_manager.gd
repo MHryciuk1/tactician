757bcf53e48c498dc302d2c28bb7bc5c1cd3bb58
sudhishc <sudhish.ch@gmail.com>
2025-04-23 18:21:17 -0500

    Update ui_manager.gd

--- Scripts/ui_manager.gd
+++ Scripts/ui_manager.gd
@@ -1,20 +1,9 @@
 extends CanvasLayer
 class_name UIManager
 
-
-#const UnitIconScene = preload("res://nodes/UI/unit.tscn")
-#
-#
-#func add_unit_icon(cord: Vector2i, hex_grid: Node) -> void:
-	#var icon = UnitIconScene.instance()
-	#icon.position = hex_grid.to_global( hex_grid.map_to_local(cord) )
-	#add_child(icon)
-#
-#func remove_unit_icon(unit: Node2D) -> void:
-	#unit.queue_free()
+@onready var move_list: VBoxContainer = $MoveOptions
 
 func send_data(ui_id: String, stats: Dictionary, moves: Array) -> void:
-	var panel: Control
 	
 	var lbl = Label.new()
 	lbl.autowrap = true
@@ -23,19 +12,18 @@ func send_data(ui_id: String, stats: Dictionary, moves: Array) -> void:
 		text += "%s: %s\n" % [ stat_name.capitalize(), stats[stat_name]]
 	lbl.text = text
 	
-	var move_list = panel.get_node("MoveOptions") as VBoxContainer
-	move_list.clear()
+	for child in move_list.get_children():
+		child.queue_free()
 	
 	move_list.add_child(lbl)
 	
 	for move in moves:
 		var b = Button.new()
 		b.text = str(move)
-		b.pressed.connect(do_move, move)
+		b.pressed.connect(Callable(self, "do_move"), [move])
 		move_list.add_child(b)
 
 func do_move(move: String) -> void:
-	var panel: Control
-	var move_list = panel.get_node("MoveOptions") as VBoxContainer
-	move_list.clear()
+	for child in move_list.get_children():
+		child.queue_free()
 	
