f9b95e711ce5c0e0fe10b40718a43b0c1429c361
Maciek Hryciuk <maciekhry@gmail.com>
2025-05-07 21:08:35 -0500

    merge fixes

--- Scripts/logic_manager.gd
+++ Scripts/logic_manager.gd
@@ -13,63 +13,13 @@ var p2vision: Array = []
 var actions_enabled : bool = false
 
 
-func init(hex_grid_ref : Hex_Grid, ui_ref : UIManager, containers : Array) -> void:
 
-var unique_id_to_unit : Dictionary[int,Unit] = {}
-var unit_scenes = [
-	load("res://Nodes/Units/Knight_Unit.tscn"),
-	load("res://Nodes/Units/Scout_Unit.tscn"),
-	load("res://Nodes/Units/archer_unit.tscn")
-]
-func send_starting_units():
-	
-	var payload = []
-	for i in unit_containers[current_player-1].get_children():
-		payload.append([i.unit_id, i.current_cord])
-	print(str(player_name, " ", unit_containers[current_player-1].get_children()))
-	server_send_starting_units.rpc(payload)
-func place_units(units) -> void:
-	for i in units.data:
-		assert(i.id !=0)
-		var new_unit = unit_scenes[i.id-1].instantiate()
-		new_unit.init(self, hex_grid, ui, i.pos, i.team)
-		new_unit.stats = i.stats
-		new_unit.moves = i.moves
-		new_unit.unique_id = i.unique_id
-		unique_id_to_unit[new_unit.unique_id] = new_unit
-		unit_containers[i.team-1].add_child(new_unit)
-		
-@rpc("authority","call_remote","unreliable_ordered")
-func set_up_response(res) -> void:
-	print(player_name," setup")
-	if res.status == 1:
-		print("Unit Error")
-	elif res.status == 0:
-		print("Units Good")
-		print(res.data)
-		ui.close_placement_pannel()
-		current_player = 2
-		var units : Array = unit_containers[0].get_children()
-		for i in range(res.data.size()):
-			units[i].unique_id = res.data[i].unique_id
-			unique_id_to_unit[units[i].unique_id] = units[i]
-	else:
-		print("Enemy Placement")
-		print(res)
-		place_units(res)
-		
-		if(res.begin_placement):
-			current_player = 1
-			ui.open_placement_pannel()
-	print(unique_id_to_unit)
-			
-@rpc("authority","call_remote","unreliable_ordered")
-func turn_start() -> void:
-	print(str("turn start ", player_name))
-	current_player = 1
-	actions_enabled = true
-	ui.open_turn_pannel()
-	hex_grid.update_vision(get_visible_coords())
+
+
+
+
+
+
 
 var unique_id_to_unit : Dictionary[int,Unit] = {}
 var unit_scenes = [
@@ -124,6 +74,7 @@ func turn_start() -> void:
 	print(str("turn start ", player_name))
 	ui.update_turn_pop_up_display(turn_number)
 	ui.show_turn_pop_up()
+	hex_grid.update_vision(get_visible_coords())
 	await get_tree().create_timer(1).timeout
 	ui.hide_turn_pop_up()
 	current_player = 1
@@ -157,21 +108,6 @@ func do_effect(source_unit: Unit, effect_name : String, targets: Array) -> void:
 	$attacksound.play()
 	# other stuff needs to happen here? like ui updates or something
 
-func move_to(unit: Unit, target_cord: Vector2i) -> void:
-	var path = hex_grid.get_hexes_along_path_to(unit.current_cord, target_cord)
-	var actual_target  : Vector2i= path.pop_front()
-	print(path)
-	print(unit.stats.move_range)
-	for i in path:
-		var hex_data : Array = hex_grid.get_cell_data(i)
-		if(unit.stats.move_range - hex_data[hex_grid.CELL_MOVE_COST]) >= 0:
-			unit.stats.move_range -= hex_data[hex_grid.CELL_MOVE_COST]
-			actual_target = i
-		else:
-			break
-	#var movable = can_move_to(unit, target_cord)
-	if unit.current_cord == actual_target:
-		print("No movement possible for ", unit.unit_type)
 
 @rpc("authority", "call_remote", "unreliable_ordered")
 func game_over(i_won : bool) -> void:
@@ -193,16 +129,17 @@ func update_unit(update : Dictionary) -> void :
 	unit._update_bar()
 	unit.moves = update.moves
 	if(update.pos != unit.current_cord):
+		hex_grid.get_cell_data(unit.current_cord)[hex_grid.CELL_OCCUPANT] = null
 		unit.current_cord = update.pos
 		if ui.move_source == unit:
 			var valid_moves = hex_grid.get_valid_spaces(unit.current_cord, unit.get_stats().attack_range,false)
 			hex_grid.highlight_hexes(valid_moves, Color.BLUE)
 		hex_grid.get_cell_data(unit.current_cord)[hex_grid.CELL_OCCUPANT] = null
 		hex_grid.set_node_location(unit, update.pos)
+		hex_grid.update_vision(get_visible_coords())
 	if(!update.alive):
 		unique_id_to_unit.erase(update.unique_id)
 		hex_grid.clear_hex(unit.current_cord)
-		
 @rpc("authority","call_remote","unreliable_ordered")
 func unit_update(update : Array[Dictionary]) -> void:
 	print(str("unit_update ", player_name, update))
@@ -210,42 +147,12 @@ func unit_update(update : Array[Dictionary]) -> void:
 		update_unit(i)
 	
 
-	unit.current_cord = actual_target
-	hex_grid.draw_line_on_grid(actual_target, hex_grid.curr_hex)
-	#need to update vision arrays and update fog based on where unit is moved to
-	print(unit.unit_type, "moved to: ", actual_target)
 
-@rpc()
-func move_request(source : int, target : Vector2i) -> void:
-	pass
-func update_unit(update : Dictionary) -> void :
-	var unit : Unit = unique_id_to_unit.get(update.unique_id)
-	if (!unit):
-		print("unit does not exist")
-		return
-		unit.stats = update.stats
-		unit.current_cord = update.pos
-	
-	unit.moves = update.moves
-	if(update.pos != unit.current_cord):
-		hex_grid.get_cell_data(unit.current_cord)[hex_grid.CELL_OCCUPANT] = null
-		hex_grid.set_node_location(unit, update.pos)
-		hex_grid.update_vision(get_visible_coords())
-@rpc("authority","call_remote","unreliable_ordered")
-func unit_update(update : Array[Dictionary]) -> void:
-	print(str("unit_update ", player_name, update))
-	for i in update:
-		update_unit(i)
-	
+
 func move_to(unit: Unit, target_cord: Vector2i) -> void:
 	print(unique_id_to_unit)
 	move_request.rpc( unit.unique_id,target_cord)
 	hex_grid.update_vision(get_visible_coords())
-
-func move_to(unit: Unit, target_cord: Vector2i) -> void:
-	print(unique_id_to_unit)
-	move_request.rpc( unit.unique_id,target_cord)
-
 	#var path = hex_grid.get_hexes_along_path_to(unit.current_cord, target_cord)
 	#var actual_target  : Vector2i= path.pop_front()
 	#print(path)
